2ce7cf3d04a4216be4083c66b362615a7664617d
Epiesteban <47354346+Epiesteban@users.noreply.github.com>
2019-03-27 13:16:59 +0100

    Nova versio mineria processos

--- mineriaambepisodiscomplerts.py
+++ mineriaambepisodiscomplerts.py
@@ -2,8 +2,12 @@
 import psycopg2 
 from sets import Set
 import numpy as np
+import functools 
+import operator
 
-
+def convertTuple(tup): 
+		pr = functools.reduce(operator.add, (tup)) 
+		return pr
 def connect():
     conn = None 
     try:
@@ -28,36 +32,39 @@ def connect():
 
             print 'Mineria'
             print 'Juntar episodis amb esdeveniments'
+	    
 	    diccionari={}
-            cur.execute("SELECT * FROM episodi;")
+            cur.execute("SELECT DISTINCT (episodiid) FROM episodi;")
 	    episodiiesdeveniment=cur.fetchall()
             #episodis=map(lambda x:list(diccionari[x[0]]).append(x[1]),episodiiesdeveniment)
 	    #print episodis
             #episodis=map(lambda x:x[0],episodiiesdeveniment)
-	    episodiiesdeveniment=map(lambda x:(x[0],x[1]),episodiiesdeveniment)
+	    episodiiesdeveniment=map(lambda x:(x[0]),episodiiesdeveniment)
 	    #print episodiiesdeveniment
 	    i=0
 	    #ho limitem a 1000 esdeveniments pero ho hem de canviar
-	   
+	    
 	    taulaaccions=['Radiografia', 'Electrocardiograma', 'Mesurament i pesatge', 'Oximetria', 'Vacuna', 'Donar piruleta', 'Enguixar extremitat', 'Analisi de sang', 'Receptar medicament', 'Posar tireta']
 
 	    matriu=np.zeros((10,10),dtype=int)
 	    print matriu
 	    for episodi in episodiiesdeveniment[:1000]:
-	        lst=[k for k in episodiiesdeveniment if episodi[0] in k]
-	   	diccionari[episodi[0]]=map(lambda x:x[1],lst)
-		esdeveniments= map(lambda x:x[1],lst)
+	        #buscar tots els esdeveniments daquest mateix episodi
+	    	cur.execute("SELECT esdevenimentid FROM episodi WHERE episodiid = '" + str(episodi) + "';")
 		accions=[]
-		#print esdeveniments
-	   	for esdeveniment in esdeveniments:	
-			cur.execute("SELECT accio FROM esdeveniment WHERE esdevenimentid = '" + str(esdeveniment) + "';")
-		    	accio=map(lambda x:x[0] ,cur.fetchall())[0]
+		#prova = convertTuple(row) 
+		#print(prova)
+		index=0
+		for row in cur.fetchall():
+			taulaaux=['taulaaccions']
+			#retorna un a un els valors
+			row= convertTuple(row)
+			cur.execute("SELECT accio FROM esdeveniment WHERE esdevenimentid = " + str(row) + ";")
+			accio=map(lambda x:x[0] ,cur.fetchall())[0]
 			accions.append(accio)
-			#print accions	   
-		if len(accions)>1:
-			print accions
+		if len(diccionari)>1:
+			#print diccionari
 			i=0
-			print accions
 			for accio in accions:
 				print accio
 				index1=taulaaccions.index(accio)
@@ -67,43 +74,9 @@ def connect():
 					i=i+1
 				except:
 					print "error brutal"
-				#print matriu([index1][index2])
+	    	diccionari[episodi]=accions
 	    print matriu
-				
-			
-	    #print diccionari
-	    
-	    '''
-	    print 'Esdeveniment inici'
-	    accionsinici=[]
-	    accionsinicinorepetits = {}
-	    esdevenimentsinici = map(lambda x:x[0], diccionari.values())
-	    for esdeveniment in esdevenimentsinici:	
-		cur.execute("SELECT accio FROM esdeveniment WHERE esdevenimentid = '" + str(esdeveniment) + "';")
-	    	episodiiesdeveniment=map(lambda x:x[0] ,cur.fetchall())[0]
-	   	accionsinici.append(episodiiesdeveniment)
-		
-	    accionsinicinorepetits=list(set(accionsinici)) 
-	    print accionsinicinorepetits
-	    
-	    print 'Esdeveniments final'
-	    accionsfinal=[]
-	    accionsfinalnorepetits={}
-	    esdevenimentsfinal=map(lambda x:x[-1], filter(lambda x:len(x)>1,diccionari.values()))
-	    for esdevenimentfinal in esdevenimentsfinal:
-		cur.execute("SELECT accio FROM esdeveniment WHERE esdevenimentid = '" + str(esdevenimentfinal) + "';")
-		episodiiesdevenimentfinal=map(lambda x:x[0] ,cur.fetchall())[0]
-		accionsfinal.append(episodiiesdevenimentfinal)
-	    print 'Llista accions finals'
-	    print accionsfinal
-	    accionsfinalnorepetits=list(set(accionsfinal))
-	    print 'Llista accions finals no repetits'
-	    print accionsfinalnorepetits
-	    '''
 
-	    
-	    
- 
         except (Exception, psycopg2.DatabaseError) as error:
             print (error)
 
